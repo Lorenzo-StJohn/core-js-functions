980d90a380ed8ef97f54e256aca0a946c0224cfb
Lorenzo-StJohn <britishaccent@bk.ru>
2025-03-16 04:08:22 +0400

    feat: implement memoize

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -109,8 +109,16 @@ function getPolynom(...arg) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  let a = false;
+  let res;
+  return (...b) => {
+    if (!a) {
+      a = true;
+      res = func(...b);
+    }
+    return res;
+  };
 }
 
 /**
